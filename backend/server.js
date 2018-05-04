b21251e9e83389b88d0313206acfa6ed34ab1ed4
Christian Bjerg <cmb@conzentrate.com>
2018-05-04 14:30:07 +0200

    mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import schema from './schema';
-
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+
+mongoose.connect('mongodb://localhost/graphqlserver');
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
 
